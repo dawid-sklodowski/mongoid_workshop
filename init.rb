2bbf3dec137c449864de6d632412913baf5ed2fb
Dawid Sklodowski <davesqu@gmail.com>
2013-07-29 10:44:04 +0100

    Loading mongoid config

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.expand_path("../lib", __FILE__))
 require 'rubygems'
 require 'bundler'
 Bundler.require :default
-#Mongoid.load!("config/mongoid.yml")
+Mongoid.load!("config/mongoid.yml")
 require 'delayed_job'
 require 'user'
 require 'property'
